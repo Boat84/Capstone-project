cc3aac88208872c64d73f97650bf464006337347
Florian <florian.schoensiegel@gmail.com>
2024-07-17 09:59:47 +0200

    correct file

--- pages/Visualization Dashboard.py
+++ pages/Visualization Dashboard.py
@@ -9,6 +9,84 @@ import tempfile
 import styles
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import plotly.express as px
+from collections import Counter
+from wordcloud import WordCloud, STOPWORDS
+import spacy
+from io import BytesIO
+from PIL import Image
+
+stop_words = set(stopwords.words('english'))
+lemmatizer = WordNetLemmatizer()
+
+# Sentiment analysis functions from Visualization_Z1.py
+def preprocess_text(text):
+    lines = text.split('\n')
+    cleaned_lines = [line for line in lines if not line.strip().startswith(('EXT', 'INT'))]
+    cleaned_text = '\n'.join(cleaned_lines)
+    cleaned_text = re.sub(r'[^\w\s]', '', cleaned_text.lower())
+    tokens = word_tokenize(cleaned_text)
+    tokens = [token for token in tokens if token not in stop_words]
+    tokens = [lemmatizer.lemmatize(token) for token in tokens]
+    processed_text = ' '.join(tokens)
+    return processed_text
+
+def classify_and_save_scenes(text):
+    scenes = text.split("==================================================")
+    analyzer = SentimentIntensityAnalyzer()
+    scene_scores = []
+    for i, scene in enumerate(scenes):
+        preprocessed_text = preprocess_text(scene)
+        scores = analyzer.polarity_scores(preprocessed_text)
+        scene_scores.append({
+            "Scene": i,
+            "Negative": scores['neg'],
+            "Neutral": scores['neu'],
+            "Positive": scores['pos'],
+            "Compound": scores['compound']
+        })
+    return scene_scores
+
+def plot_zoomable_trend_chart(data):
+    fig = px.line(data, x='Scene', y='Compound', title='Sentiment Changes Over Scenes')
+    fig.update_layout(
+        title={
+            'text': 'Sentiment Changes Over Scenes',
+            'font': {'color': 'black', 'size': 24},  # Set title color to black and increase size
+            'x': 0.5,  # Center the title
+            'xanchor': 'center'
+        },
+        xaxis_title={
+            'text': 'Scene',
+            'font': {'color': 'black', 'size': 18}  # Set x-axis title color to black and increase size
+        },
+        yaxis_title={
+            'text': 'Sentiment',
+            'font': {'color': 'black', 'size': 18}  # Set y-axis title color to black and increase size
+        },
+        autosize=True,
+        width=1745,  # Match the width of the other visualization
+        height=600,  # Match the height of the other visualization
+        margin=dict(l=20, r=20, t=40, b=20),
+        hovermode='closest',
+        xaxis=dict(
+            rangeslider=dict(visible=True),
+            type="linear"
+        ),
+        plot_bgcolor='white',  # Set plot background to white
+        paper_bgcolor='white'  # Set paper background to white
+    )
+    # Update line, marker, label colors
+    fig.update_traces(line_color='#433D8B', marker_color='#433D8B')
+    fig.update_xaxes(tickfont=dict(color='black'))  
+    fig.update_yaxes(tickfont=dict(color='black'))
+
+    return fig
 
 # Set Streamlit page configuration
 st.set_page_config(**styles.set_page_config())
@@ -18,22 +96,7 @@ st.markdown(
     """
     <style>
     .stApp {
-        background-image: url("https://img.freepik.com/free-photo/movie-background-collage_23-2149876005.jpg?w=1380&t=st=1721049219~exp=1721049819~hmac=bc526a47ba7a903e68dea18466d46565e9ca5a5bc11399f715d11c145fb87d8e");
-        background-size: cover;
-        background-repeat: no-repeat;
-        background-attachment: fixed;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-# Set overall page background image
-st.markdown(
-    """
-    <style>
-    .stApp {
-        background-image: url("https://img.freepik.com/free-photo/movie-background-collage_23-2149876005.jpg?w=1380&t=st=1721049219~exp=1721049819~hmac=bc526a47ba7a903e68dea18466d46565e9ca5a5bc11399f715d11c145fb87d8e");
+        background-image: url("https://i.ibb.co/bdVswk3/movie-background-collage-23-2149876005.jpg");
         background-size: cover;
         background-repeat: no-repeat;
         background-attachment: fixed;
@@ -64,7 +127,7 @@ st.markdown("<h1 style='text-align: center; color: white;'>Visualization Dashboa
 # Check if a file has been uploaded
 if 'uploaded_file' in st.session_state and st.session_state['uploaded_file'] is not None:
     uploaded_file = st.session_state['uploaded_file']
-    st.write(f"File ready for processing: {uploaded_file.name}")
+    st.write(f"Uploaded file: {uploaded_file.name}")
 
     # Add a button to create the visualization
     if st.button('Create Visualization'):
@@ -78,8 +141,8 @@ if 'uploaded_file' in st.session_state and st.session_state['uploaded_file'] is 
 
         # List of common uppercase expressions to exclude
         non_character_expressions = [
-            'BLACK', 'CUT TO', 'FADE OUT', 'FADE IN', 'DISSOLVE TO', 'CUT IN',
-            'MORE', 'CONT’D', 'CONTINUED', 'FADE TO BLACK', 'TITLE', 'REVEAL', 'OMITTED'
+            'BLACK', 'CUT TO', 'FADE OUT', 'FADE IN', 'DISSOLVE TO', 'CUT IN', 'CLOSE', 'BACK TO SCENE', 'ON', 'MOMENTS LATER'
+            'MORE', 'CONT’D', 'CONTINUED', 'FADE TO BLACK', 'TITLE', 'REVEAL', 'OMITTED', 'P.O.V.', 'POV', 'SUPER', 'BACK TO SCENE', 'CONT', 'EXT', 'INT'
         ]
 
         # Regex pattern to capture character dialogues
@@ -238,7 +301,6 @@ if 'uploaded_file' in st.session_state and st.session_state['uploaded_file'] is 
 
         # Function to identify scenes using DataFrame
         def identify_scenes(text, title):
-            # Compile the regular expressions
             ext_pattern = re.compile(r'\bEXT[.\:\s\-\–\,]', re.MULTILINE)
             int_pattern = re.compile(r'INT[.\:\s\-\–\,]', re.MULTILINE)
             uppercase_pattern = re.compile(r'^[A-Z0-9\s:\(\)\-\.\:\,]+$', re.MULTILINE)
@@ -248,30 +310,21 @@ if 'uploaded_file' in st.session_state and st.session_state['uploaded_file'] is 
             smash_pattern = re.compile(r'\bSMASH CUT[.\:\s\-\–\,]', re.MULTILINE)
             scene_pattern = re.compile(r'(?m)^\[Scene:?\s.*?\,\]$', re.MULTILINE)
             
-            # Split text into lines
             lines = text.splitlines()
-            
-            # Create a DataFrame from lines
             df = pd.DataFrame(lines, columns=['line'])
-            
-            # Add a column to store matches
             df['match'] = None
-            
-            # Define match function
+
             def match_line(line, pattern, match_type):
                 if pattern.search(line):
                     return match_type
                 return None
-            
-            # First pass: EXT and INT matches
+
             df['match'] = df['line'].apply(lambda x: match_line(x, ext_pattern, 'EXT') or match_line(x, int_pattern, 'INT') or match_line(x, scene_pattern, 'SCENE')
                                         or match_line(x, fade_pattern, 'FADE OUT') or match_line(x, cut_pattern, 'CUT TO'))
-            
-            # Second pass: Uppercase lines if less than 150 matches found
+
             if df['match'].count() < 150:
                 df['match'] = df.apply(lambda x: 'UPPERCASE' if (uppercase_pattern.match(x['line']) and pd.isna(x['match']) and len(x['line'].split()) >= 3) else x['match'], axis=1)
             
-            # Third pass: Fade, cut, dissolve, smash, and scene pattern matches if still less than 150 matches
             if df['match'].count() < 150:
                 df['match'] = df.apply(lambda x: (
                     'FADE OUT' if fade_pattern.search(x['line']) else
@@ -282,7 +335,6 @@ if 'uploaded_file' in st.session_state and st.session_state['uploaded_file'] is 
                     x['match']
                 ), axis=1)
             
-            # Collect matches
             matches = []
             match_counter = 1
             for index, row in df.iterrows():
@@ -292,7 +344,6 @@ if 'uploaded_file' in st.session_state and st.session_state['uploaded_file'] is 
             
             return matches
 
-        # Function to extract scenes
         def extract_scenes(text, matches):
             scenes = {}
             for i in range(len(matches)):
@@ -313,20 +364,17 @@ if 'uploaded_file' in st.session_state and st.session_state['uploaded_file'] is 
 
             return scenes
 
-        # Function to clean scene text
         def clean_scene_text(scene_text):
             lines = scene_text.splitlines()
             cleaned_lines = [re.sub(r'\s+', ' ', line.strip()) for line in lines]
             cleaned_text = "\n".join(cleaned_lines)
             return cleaned_text
 
-        # Perform scene separation
         screenplay_text = st.session_state['screenplay_text']
         title = uploaded_file.name.split('.')[0]
         scene_headings = identify_scenes(screenplay_text, title)
         scenes = extract_scenes(screenplay_text, scene_headings)
 
-        # Create Interaction Time Series graph
         scene_dialogues = []
         scene_counter = 1
 
@@ -336,35 +384,25 @@ if 'uploaded_file' in st.session_state and st.session_state['uploaded_file'] is 
                 scene_dialogues.append((f"Scene {scene_counter}", character.strip(), dialogue))
             scene_counter += 1
 
-        # Convert to DataFrame
         dialogue_df = pd.DataFrame(scene_dialogues, columns=['Scene', 'Character', 'Dialogue'])
 
-        # Identify character names
         potential_characters = character_name_pattern.findall(screenplay_text)
 
-        # Clean up character names by removing trailing spaces and newline characters
         cleaned_characters = [re.sub(r'\s+$', '', char) for char in potential_characters]
 
-        # Filter out non-character expressions
         cleaned_characters = [char for char in cleaned_characters if char not in non_character_expressions]
 
-        # Create a DataFrame to count the occurrences of each character
         character_counts = pd.Series(cleaned_characters).value_counts()
 
-        # Filter characters that appear frequently enough to be considered as actual characters
         characters = character_counts[character_counts > character_threshold].index.tolist()
 
-        # Clean up character names in the dialogue DataFrame
         dialogue_df['Character'] = dialogue_df['Character'].apply(lambda x: re.sub(r'\s+$', '', x))
 
-        # Filter dialogues to include only those with identified characters
         dialogue_df = dialogue_df[dialogue_df['Character'].isin(characters)]
 
-        # Create interaction matrix for all characters
         all_characters = dialogue_df['Character'].unique()
         interaction_matrix_all = pd.DataFrame(0, index=all_characters, columns=all_characters)
 
-        # Track interactions across scenes
         scene_interactions = []
 
         for scene, group in dialogue_df.groupby('Scene'):
@@ -379,62 +417,129 @@ if 'uploaded_file' in st.session_state and st.session_state['uploaded_file'] is 
                     interaction_matrix_all.loc[char2, char1] += 1
             scene_interactions.append((scene, scene_matrix.sum().sum()))
 
-        # Convert scene interactions to DataFrame
         scene_interactions_df = pd.DataFrame(scene_interactions, columns=['Scene', 'Interaction Count'])
 
-        # faking it now
         scene_interactions_df['Interaction Count'] = scene_interactions_df['Interaction Count'] // 10
 
-        # Reset the scene numbers to start from 1
         scene_interactions_df['Scene'] = [f"Scene {i+1}" for i in range(len(scene_interactions_df))]
 
-        # Create the interactive plot
         fig = make_subplots(rows=1, cols=1)
 
-        # Add the line plot
         fig.add_trace(
             go.Scatter(x=scene_interactions_df['Scene'], 
                     y=scene_interactions_df['Interaction Count'], 
                     mode='lines+markers',
                     name='Interactions',
-                    line=dict(color='#433D8B'),  # Set the line color
-                    marker=dict(color='#433D8B'),  # Set the marker color
+                    line=dict(color='#433D8B'),  
+                    marker=dict(color='#433D8B'),  
                     hovertemplate='Scene: %{x}<br>Interactions: %{y}<extra></extra>')
         )
 
-        # Update layout
         fig.update_layout(
             title={
             'text': 'Character Interactions Over Scenes',
-            'font': {'color': 'black'},  # Set title color to black
-            'x': 0.5,  # Center the title
+            'font': {'color': 'black'},  
+            'x': 0.5,  
             'xanchor': 'center'
             },
             xaxis_title={
             'text': 'Scene',
-            'font': {'color': 'black'}  # Set x-axis title color to black
+            'font': {'color': 'black'}  
             },
             yaxis_title={
             'text': 'Interaction Count',
-            'font': {'color': 'black'}  # Set y-axis title color to black
+            'font': {'color': 'black'}  
             },
             hovermode='closest',
-            autosize=True,  # Let Plotly handle the sizing
+            autosize=True,  
             width=1745,
             height=600,
-            plot_bgcolor='white',  # Set the plot background color to white
-            paper_bgcolor='white',  # Set the paper background color to white
+            plot_bgcolor='white',  
+            paper_bgcolor='white',  
         )
 
-        # Update x-axis
-        fig.update_xaxes(tickangle=45, tickfont=dict(color='black'))  # Set x-axis tick color to black
+        fig.update_xaxes(tickangle=45, tickfont=dict(color='black'))  
+        fig.update_yaxes(tickfont=dict(color='black'))  
 
-        # Update y-axis
-        fig.update_yaxes(tickfont=dict(color='black'))  # Set y-axis tick color to black
-
-
-        # Display the plot in Streamlit
         st.plotly_chart(fig)
 
+        # Add the sentiment graph here
+        scene_separated_text = "==================================================".join(scene_content for scene_title, scene_content in scenes.items())
+        processed_results = classify_and_save_scenes(scene_separated_text)
+        sentiment_data = pd.DataFrame(processed_results)
+        st.plotly_chart(plot_zoomable_trend_chart(sentiment_data), use_container_width=True)
+
+        # Custom color function
+        def custom_color_func(word, font_size, position, orientation, random_state=None, **kwargs):
+            colors = ["#17153B", "#2E236C", "#433D8B", "#C8ACD6"]
+            return colors[random_state.randint(0, len(colors) - 1)]
+
+        def create_word_cloud(file_contents):
+            # Use charset_normalizer to detect encoding
+            result = from_bytes(file_contents).best()
+            script_text = str(result)
+
+            # Remove Directorial Expressions and Character Names
+            directorial_expressions = [
+                'BLACK', 'CUT TO', 'FADE OUT', 'FADE IN', 'DISSOLVE TO', 'CUT IN', 'CLOSE',
+                'MORE', 'CONT’D', 'CONTINUED', 'FADE TO BLACK', 'TITLE', 'REVEAL', 'OMITTED', 'P.O.V.', 'POV', 'SUPER', 'BACK TO SCENE', 'CONT', 'EXT', 'INT'
+            ]
+
+            for expression in directorial_expressions:
+                script_text = script_text.replace(expression, '')
+
+            character_name_pattern = re.compile(r'\n\s*([A-Z][A-Z\s]+)\s*\n')
+            script_text = re.sub(character_name_pattern, '', script_text)
+
+            additional_stopwords = set([
+                'the', 'and', 'is', 'in', 'to', 'with', 'that', 'on', 'for', 'as', 'it',
+                'of', 'at', 'by', 'this', 'be', 'which', 'or', 'from', 'an', 'but', 'not',
+                'we', 'you', 'your', 'so', 'can', 'are', 'if', 'then', 'will', 'there', 
+                'he', 'she', 'they', 'what', 'all', 'one', 'out', 'up', 'would', 'his', 
+                'her', 'their', 'my', 'me', 'no', 'do', 'when', 'about', 'just', 'more', 
+                'how', 'like', 'who', 'did', 'them', 'now', 'him', 'said', 'get', 'got',
+                'something', 'anything', 'everything', 'somebody', 'anybody', 'us', 'we'
+            ])
+
+            stopwords = STOPWORDS.union(additional_stopwords)
+
+            filtered_words = [word for word in script_text.split() if word.lower() not in stopwords and len(word) >= 3]
+            filtered_text = ' '.join(filtered_words)
+
+            nlp = spacy.load('en_core_web_sm')
+            doc = nlp(filtered_text)
+
+            names = set()
+            for ent in doc.ents:
+                if ent.label_ in ['PERSON']:
+                    names.add(ent.text)
+
+            # Filter out verbs and names from the tokens
+            filtered_tokens = [token.text for token in doc if token.text not in names and len(token.text) >= 3 and token.pos_ != 'VERB']
+
+            # Count the frequency of tokens
+            token_counts = Counter(filtered_tokens)
+
+            # Select the 25 most common tokens
+            most_common_tokens = token_counts.most_common(50)
+
+            # Join the most common tokens into a single string for word cloud generation
+            wordcloud_text = ' '.join([token for token, count in most_common_tokens])
+
+            return wordcloud_text
+
+        file_contents = screenplay.encode(encoding="utf-8")
+        wordcloud_text = create_word_cloud(file_contents)
+
+        if wordcloud_text:
+            # Create a high-resolution word cloud
+            wordcloud = WordCloud(stopwords=STOPWORDS, background_color='white', color_func=custom_color_func, width=1920, height=1080).generate(wordcloud_text)
+
+            # Convert to image
+            image = wordcloud.to_image()
+
+            # Display the word cloud using Streamlit
+            st.image(image, use_column_width=True)
+
     else:
-        st.write("Please upload a text file to visualize the character interaction network.")
+        st.write("")
